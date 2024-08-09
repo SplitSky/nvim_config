a9410d3699f9b1b83a438fb2b1ebcdf45ab782bd
SplitSky <opop.neska60@gmail.com>
2024-08-09 23:00:41 +0100

    Added more config

--- init.lua
+++ init.lua
@@ -835,3 +835,9 @@ require("lazy").setup({
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
+
+-- set tab stop size
+local set = vim.opt
+set.tabstop = 4
+set.softtabstop = 4
+set.shiftwidth = 4
