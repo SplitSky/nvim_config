20de41859aba85bf345f6c40a65ed7f9666486bb
SplitSky <opop.neska60@gmail.com>
2024-08-22 13:07:47 +0100

    Added autocmd svelte files

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -2,7 +2,7 @@
 
 -- Auto source Lua files on save
 vim.cmd("autocmd BufWritePost *.lua source %")
-
+vim.cmd("autocmd BufWritePost *.svelte source %")
 -- Highlight yanked text
 vim.cmd("autocmd TextYankPost * silent! lua vim.highlight.on_yank {on_visual = false}")
 
