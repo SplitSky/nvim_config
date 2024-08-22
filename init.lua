b7d5b3db05b19e82da4deb44abe1773f8f50ec89
SplitSky <opop.neska60@gmail.com>
2024-08-22 10:39:59 +0100

    Appended the lazy loader instead of packer

--- init.lua
+++ init.lua
@@ -1,25 +1,39 @@
 -- init.lua
 
--- Load core settings
-require("settings")
+-- Ensure LazyVim is installed
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
 
--- Load plugins
-require("plugins")
+-- Load LazyVim
+require("lazy").setup("plugins")
+
+-- Load core settings
+require('settings')
 
 -- Load keybindings
-require("keybindings")
+require('keybindings')
 
 -- Load LSP configurations
-require("lsp_config")
+require('lsp_config')
 
 -- Load Autocommands
-require("autocmds")
+require('autocmds')
 
 -- Load Themes and Appearance
-require("themes")
+require('themes')
 
 -- Load language-specific configurations
-require("languages.python")
-require("languages.typescript")
-require("languages.cpp")
-require("languages.svelte")
+require('languages.python')
+require('languages.typescript')
+require('languages.cpp')
+require('languages.svelte')
