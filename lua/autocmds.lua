c41c08f59a66807c126265c95154655407379aab
SplitSky <opop.neska60@gmail.com>
2024-08-24 17:44:54 +0100

    Fixed rebase

--- lua/autocmds.lua
+++
@@ -1,8 +0,0 @@
--- ~/.config/nvim/lua/autocmds.lua
-
--- Auto source Lua and Svelte files on save
-vim.cmd("autocmd BufWritePost *.lua source %")
-vim.cmd("autocmd BufWritePost *.svelte source %")
-
--- Highlight yanked text
-vim.cmd("autocmd TextYankPost * silent! lua vim.highlight.on_yank {on_visual = false}")
--- lua/keybindings.lua
+++
@@ -1,9 +0,0 @@
--- ~/.config/nvim/lua/keybindings.lua
-
-vim.g.mapleader = " " -- Space is the leader key
-
--- Telescope keybindings
-vim.api.nvim_set_keymap("n", "<leader>f", ":Telescope find_files<CR>", { noremap = true, silent = true })
-
--- Terminal toggle
-vim.api.nvim_set_keymap("n", "<leader>t", ":ToggleTerm<CR>", { noremap = true, silent = true })
