452af848c0af9ac067a8d03220883b7363083553
SplitSky <opop.neska60@gmail.com>
2025-10-09 09:52:02 +0100

    changed the lint settings to fix repricated steps. Changed the setup for the clipboard. Need to revisit it again at some point

--- init.lua
+++ init.lua
@@ -28,9 +28,7 @@ vim.opt.showmode = false
 --  Schedule the setting after `UiEnter` because it can increase startup-time.
 --  Remove this option if you want your OS clipboard to remain independent.
 --  See `:help 'clipboard'`
-vim.schedule(function()
-	vim.opt.clipboard = "unnamedplus"
-end)
+vim.opt.clipboard = "unnamedplus"
 
 -- Enable break indent
 vim.opt.breakindent = true
@@ -523,18 +521,15 @@ require("lazy").setup({
 			--  - settings (table): Override the default settings passed when initializing the server.
 			--        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
 			local servers = {
+				marksman = {},
 				clangd = { cmd = { "/usr/bin/clangd" } },
 				-- gopls = {},
 				pyright = {},
 				rust_analyzer = {},
-				-- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
-				--    https://github.com/pmizio/typescript-tools.nvim
-				-- Web Dev languages set up
 				tailwindcss = {},
 				cssls = { settings = { css = { lint = { unknownAtRules = "ignore" } } } },
 				svelte = {},
 				html = {},
-
 				lua_ls = {
 					-- cmd = {...},
 					-- filetypes = { ...},
@@ -817,19 +812,19 @@ require("lazy").setup({
 				"html",
 				"lua",
 				"luadoc",
-				"markdown",
-				"markdown_inline",
 				"query",
 				"vim",
 				"vimdoc",
-				"cpp",
-				"svelte",
-				"css",
 				"json",
 				"dockerfile",
 				"xml",
 				"javascript",
+				"rust",
+				"python",
+				"svelte",
 				"typescript",
+				"css",
+				"cpp",
 			},
 			-- Autoinstall languages that are not installed
 			auto_install = true,
@@ -840,7 +835,7 @@ require("lazy").setup({
 				--  the list of additional_vim_regex_highlighting and disabled languages for indent.
 				additional_vim_regex_highlighting = { "ruby" },
 			},
-			indent = { enable = true, disable = { "ruby" } },
+			indent = { enable = true, disable = { "ruby" }},
 		},
 		-- There are additional nvim-treesitter modules that you can use to interact
 		-- with nvim-treesitter. You should go explore a few and see what interests you:
